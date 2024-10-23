4bb44d0970674c295256a4acb7390b5d457e26f1
arnavsawant9 <arnavsawant2004@gmail.com>
2024-10-23 06:48:33 +0530

    made some changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,3 +52,31 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
+
+// // Navbar.jsx
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// function Navbar() {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <Link to="/">Home</Link>
+//         </li>
+//         <li>
+//           <Link to="/contact">Contact</Link>
+//         </li>
+//         <li>
+//           <Link to="/projects">Projects</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
